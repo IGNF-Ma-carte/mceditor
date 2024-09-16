b6213297d19232f35d7ee2e041ca19ca8834062d
viglino <jean-marc.viglino@ign.fr>
2024-09-16 14:13:26 +0200

    [UPD] Support for Multiple Sources in Map Styles

--- src/layerShop/layers/addLayerFileMVT.js
+++ src/layerShop/layers/addLayerFileMVT.js
@@ -2,16 +2,28 @@ import dialog from "mcutils/dialog/dialog";
 import dialogImportFile from "mcutils/dialog/dialogImportFile";
 import MVT from "mcutils/layer/MVT";
 import { insertLayer } from "./loadLayer";
+import { MapLibreLayer } from '@geoblocks/ol-maplibre-layer';
 
 function addLayerFileMVT() {
   dialogImportFile((result) => {
     try {
       const data = JSON.parse(result.data);
-      const layer = new MVT({
-        title: data.name
-      })
+      let layer;
+      if (data && Object.keys(data.sources).length > 1) {
+        layer = new MapLibreLayer({
+          title: data.name,
+          mapLibreOptions: {
+            style: data
+          },
+          attributions: data.copyright
+        });
+      } else {
+        layer = new MVT({
+          title: data.name
+        })
+        layer.applyStyle(data);
+      }
       layer.set('style', result.data);
-      layer.applyStyle(data);
       insertLayer(layer);
     } catch(e) {
       dialog.showAlert('Impossible de charger ce type de fichier...')
